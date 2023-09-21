a72614b7d641230dc48202d5294ba09e735197ac
Park Chan Hee <charlie8794@gmail.com>
2023-09-21 11:42:24 +0900

    boy_print_complete

--- character_grass.py
+++ character_grass.py
@@ -1,7 +1,15 @@
 from pico2d import *
+import math
+
 
 open_canvas()
 
-# fill here
+character = load_image('character.png')
+grass = load_image('grass.png')
+
+clear_canvas_now()
+grass.draw_now(400,30)
+character.draw_now(400,90)
+delay(1)
 
 close_canvas()
